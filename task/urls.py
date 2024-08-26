4f36c32dac0dd9901335b3830d2c2bbf55740261
Thiago Costa <thiagoocdiniz@gmail.com>
2024-08-26 20:15:17 -0300

    criando urls e views

--- task/urls.py
+++ task/urls.py
@@ -2,12 +2,7 @@ from django.urls import path
 
 from . import views
 
-app_name = 'tasks'
-
 urlpatterns = [
-    path('', views.list_tasks, name='list'),
-    path('login/', views.login, name='login'),
-    path('create/', views.create_task, name='create'),
-    path('edit/<int:pk>/', views.edit_task, name='edit'),
-    path('delete/<int:task_id>/', views.delete_task, name='delete'),
+    path('', views.task_list, name='task_list'),
+    path('create/', views.create_task, name='create_task'),
 ]
--- task/views.py
+++ task/views.py
@@ -1,47 +1,20 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import redirect, render
 
+from .forms import TaskForm
 from .models import Task
-
-
-def login(request):
-    return render(request, 'registration/login.html')
 
 
 def create_task(request):
     if request.method == 'POST':
-        title = request.POST.get('title')
-        description = request.POST.get('description')
-        due_date = request.POST.get('due_date')
-        task = Task(title=title, description=description,
-                    due_date=due_date, user=request.user)
-        task.save()
-        return redirect('tasks:list')
-    return render(request, 'tasks/create.html')
-
-
-def edit_task(request, pk):
-    task = get_object_or_404(Task, id=pk)
-    if request.method == 'POST':
-        task.title = request.POST.get('title')
-        task.description = request.POST.get('description')
-        task.due_date = request.POST.get('due_date')
-        task.save()
-        return redirect('tasks:list')
-    return render(request, 'tasks/edit.html', {'task': task})
-
-
-@login_required
-def list_tasks(request):
-    if request.user.is_authenticated:
-        tasks = Task.objects.filter(user=request.user)
-        return render(request, 'task/list.html', {'tasks': tasks})
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
     else:
+        form = TaskForm()
+    return render(request, 'task/create_task.html', {'form': form})
 
-        return redirect('login')
 
-
-def delete_task(request, task_id):
-    task = get_object_or_404(Task, id=task_id)
-    task.delete()
-    return redirect('tasks:list')
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'task/task_list.html', {'tasks': tasks})
